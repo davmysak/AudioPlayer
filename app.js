ca4dcce6318fae34b5c625e0a0777b6dcf77f6d1
David Mysak <davmysak@gmail.com>
2023-05-28 21:59:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 class MyComponent extends HTMLElement {
     constructor() {
         super();
-        // Create a shadow root
         this.attachShadow({
             mode: 'open'
         });
+        this.handleClick = this.handleClick.bind(this);
     }
     connectedCallback() {
         this.render();
@@ -15,24 +15,21 @@ class MyComponent extends HTMLElement {
       <style>
         /* Add your component styles here */
         :host {
-          display: block; /* Ensures the component is rendered as a block-level element */
+          display: block;
         }
       </style>
       <div>
         <!-- Add your component's HTML structure here -->
-        <slot></slot> <!-- Allows content insertion from outside the component -->
+        <slot></slot>
       </div>
     `;
     }
     attachEventListeners() {
-        // Add event listeners for your component's functionality
-        // For example:
-        this.shadowRoot.querySelector('div').addEventListener('click', this.handleClick);
+        const divElement = this.shadowRoot.querySelector('div');
+        divElement.addEventListener('click', this.handleClick);
     }
     handleClick(event) {
         // Handle the click event
-        // You can access the component instance with `this`
     }
-    // Other component methods and functionality
 }
 customElements.define('my-component', MyComponent);
