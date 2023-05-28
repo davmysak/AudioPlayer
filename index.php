f5328afa5fe99548356c4866d5415fdd4a897eea
David Mysak <davmysak@gmail.com>
2023-05-28 22:03:50 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Audio Player Web Component">
+    <title>Audio Player Web Component</title>
+</head>
+<body>
+    <script src="app.js" async defer type="text/javascript"></script>
+</body>
+</html>
