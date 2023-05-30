442ec8f164e5e4d9d5812709c37812d5e7d6ac13
David Mysak <davmysak@gmail.com>
2023-05-30 17:52:25 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 class AudioPlayer extends HTMLElement {
-    constructor() {
-        super();
-        this.attachShadow({
-            mode: 'open'
-        });
-        this.handleClick = this.handleClick.bind(this);
-    }
-    connectedCallback() {
-        this.render();
-        this.attachEventListeners();
-    }
-    render() {
-        this.shadowRoot.innerHTML = `
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+
+  connectedCallback() {
+    this.render();
+    this.attachEventListeners();
+  }
+
+  render() {
+    this.shadowRoot.innerHTML = `
       <style>
-        /* Add your component styles here */
         :host {
           display: block;
         }
       </style>
       <div>
-        <!-- Add your component's HTML structure here -->
         <slot></slot>
       </div>
     `;
-    }
-    attachEventListeners() {
-        const divElement = this.shadowRoot.querySelector('div');
-        divElement.addEventListener('click', this.handleClick);
-    }
-    handleClick(event) {
-        // Handle the click event
-    }
+  }
+
+  attachEventListeners() {
+    const divElement = this.shadowRoot.querySelector('div');
+    divElement.addEventListener('click', this.handleClick.bind(this));
+  }
+
+  handleClick(event) {
+    // Handle the click event
+  }
 }
-customElements.define('AudioPlayer', AudioPlayer);
+
+customElements.define('audio-player', AudioPlayer);
