ebdf79d53381425a38a6f1ac7b5f575caa77ae64
David Mysak <davmysak@gmail.com>
2023-05-30 17:54:13 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="Audio Player Web Component">
     <title>Audio Player Web Component</title>
+    <script src="app.js" type="text/javascript"></script> <!-- async defer -->
 </head>
 <body>
-    <script src="app.js" async defer type="text/javascript"></script>
+    <audio-player>
+      <audio src="<!-- audio file -->" controls></audio>
+    </audio-player>
+
+    <script>
+      // Create an instance of the audio-player component
+      const audioPlayer = document.createElement('audio-player');
+      // Append the component to a specific location in the document
+      document.body.appendChild(audioPlayer);
+    </script>
 </body>
 </html>
